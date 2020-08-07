e7697063e5d1daa8f7790e67d0727221d0eec923
Rudy Jauregui <rjaureguijr@gmail.com>
2020-08-07 12:31:04 -0500

    Edited fetch to get response and build on table in body...rj

--- app/entry.js
+++ app/entry.js
@@ -1,1 +1,23 @@
 import './styles';
+// import { response } from '../server/app';
+
+const url = 'http://localhost:8080/events';
+
+console.log(url);
+
+fetch(url)
+.then( (response) => 
+    response.json()           
+)
+.then( (data) => {
+    console.warn(data)
+    console.info(data.length)
+    for( let i = 0; i < data.length; i++ ) {
+    console.info(data[i]);
+    document.getElementById('tableBody').innerHTML +=
+    `<tr><td>${data[i].name}<td><td> ${data[i].date} <td><td> ${data[i].venue.name} <td><td> ${data[i].venue.city} <td><td> ${data[i].venue.state} <td></tr>`
+    }
+    })
+.catch(err => {
+throw new Error('An error occurred!!!')
+});
